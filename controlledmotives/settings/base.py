13e2ca3301a826f1156a0bba46167bc78d213a67
Walter Nyika <walternyika20@gmail.com>
2025-05-09 14:58:05 +0530

    Update base.py

--- controlledmotives/settings/base.py
+++ controlledmotives/settings/base.py
@@ -45,6 +45,18 @@ AUTHENTICATION_BACKENDS = (
     'allauth.account.auth_backends.AuthenticationBackend',
 )
 
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
+
+]
 # Database configuration
 DATABASES = {
     'default': {
