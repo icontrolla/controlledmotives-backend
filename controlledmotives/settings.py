5c1ee6d4c5ba6c27aa6a9dddb6c58f57beed1710
Controlled Motives <controlled@example.com>
2025-05-12 22:59:14 +0530

    troller

--- controlledmotives/settings.py
+++ controlledmotives/settings.py
@@ -114,11 +114,11 @@ DATABASES = {
 REDIS_URL = config('REDIS_URL', default='redis://localhost:6379/0')
 
 # Stripe API keys
-#STRIPE_SECRET_KEY = config('STRIPE_SECRET_KEY')
-#STRIPE_PUBLISHABLE_KEY = config('STRIPE_PUBLISHABLE_KEY')
+STRIPE_SECRET_KEY = config('STRIPE_SECRET_KEY')
+STRIPE_PUBLISHABLE_KEY = config('STRIPE_PUBLISHABLE_KEY')
 
-# NFT Storage API key
-#NFT_STORAGE_API_KEY = config('NFT_STORAGE_API_KEY')
+
+NFT_STORAGE_API_KEY = config('NFT_STORAGE_API_KEY')
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
@@ -134,8 +134,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = config('EMAIL_HOST', default='smtp.gmail.com')
 EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
 EMAIL_USE_TLS = True
-#EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-#EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
 # Cache settings (optional, if using Redis for caching)
 CACHES = {
