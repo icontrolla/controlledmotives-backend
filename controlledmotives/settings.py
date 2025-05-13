00761df0dca7c50b44b736fc5471fe21d83c861e
Controlled Motives <controlled@example.com>
2025-05-13 14:13:01 +0530

    kick it

--- controlledmotives/settings.py
+++ controlledmotives/settings.py
@@ -3,7 +3,7 @@ import os
 from decouple import config
 
 
-DEBUG = True
+DEBUG = False
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
