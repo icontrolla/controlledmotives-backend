3319725ca66c6a6427697ba18d46d122129140c7
Controlled Motives <controlled@example.com>
2025-05-15 06:44:20 +0530

    go bruh

--- controlledmotives/settings.py
+++ controlledmotives/settings.py
@@ -161,7 +161,7 @@ ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_EMAIL_VERIFICATION = "optional"  # or "mandatory" if you want email verification
 SOCIALACCOUNT_QUERY_EMAIL = True
 SOCIALACCOUNT_EMAIL_REQUIRED = True
-SOCIALACCOUNT_ADAPTER = 'controlledmotives.adapter.MySocialAccountAdapter'
+SOCIALACCOUNT_ADAPTER = 'profiles.adapter.MySocialAccountAdapter'
 
 SOCIALACCOUNT_PROVIDERS = {
     'google': {
