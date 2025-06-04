4095c0809aecbab7f3ff1d4cbe8bcc0e75de4722
Controlled Motives <controlled@example.com>
2025-06-05 03:04:18 +0530

    lordknws

--- controlledmotives/settings.py
+++ controlledmotives/settings.py
@@ -67,6 +67,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'frontend/static')]
 
+CSP_SCRIPT_SRC = ("'self'", "'unsafe-eval'")
 
 
 # CORS settings
